9e480ec20d7a40870ec1173ac3ae6970c2e5aa06
Tyler Mayoff <tyler@tylermayoff.com>
2023-01-29 00:34:26 -0500

    Cleaned up Signup/Signin code

--- recurr-frontend/src/auth.rs
+++ recurr-frontend/src/auth.rs
@@ -1,11 +1,13 @@
 use supabase_js_rs::Credentials;
-use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::spawn_local;
-use web_sys::{HtmlInputElement, InputEvent, SubmitEvent};
-use yew::{function_component, html, use_context, use_state, Callback, Html};
-use yew_hooks::use_toggle;
+use web_sys::{HtmlInputElement, SubmitEvent};
+use yew::{
+    function_component, html, Callback, Component, Context, ContextHandle, Html, NodeRef,
+    Properties,
+};
+use yew_hooks::{use_toggle, UseToggleHandle};
 
-use crate::context::SessionContext;
+use crate::context::Session;
 
 #[derive(PartialEq)]
 enum SignupSignin {
@@ -13,61 +15,205 @@ enum SignupSignin {
     Signin,
 }
 
+#[derive(Properties, PartialEq)]
+struct FormProps {
+    toggle: UseToggleHandle<SignupSignin>,
+}
+
+enum ComponentMsg {
+    SessionContextUpdate(Session),
+}
+
+struct LoginComponent {
+    context: Session,
+    _context_listener: ContextHandle<Session>,
+
+    email: NodeRef,
+    password: NodeRef,
+}
+
+impl Component for LoginComponent {
+    type Message = ComponentMsg;
+    type Properties = FormProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let (session, _context_listener) = ctx
+            .link()
+            .context(ctx.link().callback(ComponentMsg::SessionContextUpdate))
+            .expect("No message context provided");
+
+        Self {
+            context: session,
+            _context_listener,
+            email: NodeRef::default(),
+            password: NodeRef::default(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            ComponentMsg::SessionContextUpdate(session) => {
+                self.context = session;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let email_ref = self.email.clone();
+        let pass_ref = self.password.clone();
+
+        let client = self
+            .context
+            .supabase_client
+            .clone()
+            .expect("Must have supabase client already");
+
+        let signin = move |event: SubmitEvent| {
+            event.prevent_default();
+            let email = email_ref.cast::<HtmlInputElement>().unwrap().value();
+            let password = pass_ref.cast::<HtmlInputElement>().unwrap().value();
+
+            let client = client.clone();
+
+            spawn_local(async move {
+                let _ = client
+                    .auth()
+                    .sign_in_with_password(Credentials { email, password })
+                    .await;
+            });
+        };
+
+        let toggle = ctx.props().toggle.clone();
+        let toggle_form = { Callback::from(move |_| toggle.toggle()) };
+
+        html! {
+        <>
+            <h1 class="is-size-3">{"Sign In"}</h1>
+            <form onsubmit={signin}>
+                <div class="field">
+                    <label class="label">{"Email"}</label>
+                    <div class="control">
+                        <input ref={self.email.clone()} class="input" type="email" placeholder="username"/>
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">{"Password"}</label>
+                    <div class="control">
+                        <input ref={self.password.clone()} class="input" type="password" placeholder="password"/>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-link">{"Login"}</button>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <a onclick={toggle_form}>{"Don't have an account?"}</a>
+                    </div>
+                </div>
+            </form>
+        </>
+        }
+    }
+}
+
+struct SignupComponent {
+    context: Session,
+    _context_listener: ContextHandle<Session>,
+
+    email: NodeRef,
+    password: NodeRef,
+    confirm_password: NodeRef,
+}
+
+impl Component for SignupComponent {
+    type Message = ComponentMsg;
+    type Properties = FormProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let (session, _context_listener) = ctx
+            .link()
+            .context(ctx.link().callback(ComponentMsg::SessionContextUpdate))
+            .expect("No message context provided");
+
+        Self {
+            context: session,
+            _context_listener,
+            email: NodeRef::default(),
+            password: NodeRef::default(),
+            confirm_password: NodeRef::default(),
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let email_ref = self.email.clone();
+        let pass_ref = self.password.clone();
+        let conf_pass_ref = self.confirm_password.clone();
+
+        let client = self
+            .context
+            .supabase_client
+            .clone()
+            .expect("Must have supabase client already");
+
+        let signup = move |event: SubmitEvent| {
+            event.prevent_default();
+            let email = email_ref.cast::<HtmlInputElement>().unwrap().value();
+            let password = pass_ref.cast::<HtmlInputElement>().unwrap().value();
+            let confirm_password = conf_pass_ref.cast::<HtmlInputElement>().unwrap().value();
+
+            // TODO Check password
+
+            let client = client.clone();
+
+            spawn_local(async move {
+                let _ = client
+                    .auth()
+                    .sign_in_with_password(Credentials { email, password })
+                    .await;
+            });
+        };
+
+        let toggle = ctx.props().toggle.clone();
+        let toggle_form = { Callback::from(move |_| toggle.toggle()) };
+
+        html! {
+        <>
+            <h1 class="is-size-3">{"Sign Up"}</h1>
+            <form onsubmit={signup}>
+                <div class="field">
+                    <label class="label">{"Email"}</label>
+                    <div class="control">
+                        <input ref={self.email.clone()} class="input" type="email" placeholder="username"/>
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">{"Password"}</label>
+                    <div class="control">
+                        <input ref={self.password.clone()} class="input" type="password" placeholder="password"/>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-link">{"Login"}</button>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <a onclick={toggle_form}>{"Don't have an account?"}</a>
+                    </div>
+                </div>
+            </form>
+        </>
+        }
+    }
+}
+
 #[function_component(Auth)]
 pub fn auth() -> Html {
-    // TODO this function could do with a cleanup
-    let context = use_context::<SessionContext>().expect("No context found");
-
-    let signin_signup = use_toggle(SignupSignin::Signup, SignupSignin::Signin);
-
-    let email_input_value_handle = use_state(String::default);
-    let email_input_value = (*email_input_value_handle).clone();
-    let email_input_onchange: Callback<InputEvent> = {
-        let email_input_value_handle = email_input_value_handle;
-
-        Callback::from(move |event: InputEvent| {
-            let target = event.target();
-            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
-            if let Some(input) = input {
-                email_input_value_handle.set(input.value());
-            }
-        })
-    };
-
-    let password_input_value_handle = use_state(String::default);
-    let password_input_value = (*password_input_value_handle).clone();
-
-    let password_input_onchange: Callback<InputEvent> = {
-        let password_input_value_handle = password_input_value_handle;
-
-        Callback::from(move |event: InputEvent| {
-            let target = event.target();
-            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
-            if let Some(input) = input {
-                password_input_value_handle.set(input.value());
-            }
-        })
-    };
-
-    let signup = move |event: SubmitEvent| {};
-
-    let signin = move |event: SubmitEvent| {
-        event.prevent_default();
-
-        let context = context.clone();
-        let password = email_input_value.clone();
-        let email = email_input_value.clone();
-
-        spawn_local(async move {
-            let _ = context
-                .supabase_client
-                .clone()
-                .expect("Must have supabase client")
-                .auth()
-                .sign_in_with_password(Credentials { email, password })
-                .await;
-        });
-    };
+    let signin_signup = use_toggle(SignupSignin::Signin, SignupSignin::Signup);
 
     html! {
         <div class="hero is-fullheight is-flex is-justify-content-center is-align-content-center is-align-items-center">
@@ -75,55 +221,9 @@ pub fn auth() -> Html {
 
                 {
                     if (*signin_signup) == SignupSignin::Signin {
-                       html!{
-                        <>
-                            <h1 class="is-size-3">{"Sign In"}</h1>
-                            <form onsubmit={signin}>
-                                <div class="field">
-                                    <label class="label">{"Email"}</label>
-                                    <div class="control">
-                                        <input oninput={email_input_onchange} class="input" type="email" placeholder="username"/>
-                                    </div>
-                                </div>
-                                <div class="field">
-                                    <label class="label">{"Password"}</label>
-                                    <div class="control">
-                                        <input oninput={password_input_onchange} class="input" type="password" placeholder="password" value={password_input_value}/>
-                                    </div>
-                                </div>
-                                <div class="field">
-                                    <div class="control">
-                                        <button class="button is-link">{"Login"}</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </>
-                        }
+                       html!{ <LoginComponent toggle={signin_signup}/> }
                     } else {
-                        html!{
-                            <>
-                                <h1 class="is-size-3">{"Sign Up"}</h1>
-                                <form onsubmit={signin}>
-                                    <div class="field">
-                                        <label class="label">{"Email"}</label>
-                                        <div class="control">
-                                            <input oninput={email_input_onchange} class="input" type="email" placeholder="username"/>
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <label class="label">{"Password"}</label>
-                                        <div class="control">
-                                            <input oninput={password_input_onchange} class="input" type="password" placeholder="password" value={password_input_value}/>
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="control">
-                                            <button class="button is-link">{"Login"}</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </>
-                            }
+                        html!{ <SignupComponent toggle={signin_signup}/> }
                     }
                 }
 
