80f743e244af61b2f9e021209496a7104a9d5444
Tyler Mayoff <tyler@tylermayoff.com>
2023-01-28 21:11:47 -0500

    started on signup UI

--- recurr-frontend/src/auth.rs
+++ recurr-frontend/src/auth.rs
@@ -3,16 +3,25 @@ use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::{HtmlInputElement, InputEvent, SubmitEvent};
 use yew::{function_component, html, use_context, use_state, Callback, Html};
+use yew_hooks::use_toggle;
 
 use crate::context::SessionContext;
 
+#[derive(PartialEq)]
+enum SignupSignin {
+    Signup,
+    Signin,
+}
+
 #[function_component(Auth)]
 pub fn auth() -> Html {
+    // TODO this function could do with a cleanup
     let context = use_context::<SessionContext>().expect("No context found");
+
+    let signin_signup = use_toggle(SignupSignin::Signup, SignupSignin::Signin);
 
     let email_input_value_handle = use_state(String::default);
     let email_input_value = (*email_input_value_handle).clone();
-    let email = email_input_value;
     let email_input_onchange: Callback<InputEvent> = {
         let email_input_value_handle = email_input_value_handle;
 
@@ -27,7 +36,6 @@ pub fn auth() -> Html {
 
     let password_input_value_handle = use_state(String::default);
     let password_input_value = (*password_input_value_handle).clone();
-    let password = password_input_value.clone();
 
     let password_input_onchange: Callback<InputEvent> = {
         let password_input_value_handle = password_input_value_handle;
@@ -41,12 +49,14 @@ pub fn auth() -> Html {
         })
     };
 
+    let signup = move |event: SubmitEvent| {};
+
     let signin = move |event: SubmitEvent| {
         event.prevent_default();
 
         let context = context.clone();
-        let password = password.clone();
-        let email = email.clone();
+        let password = email_input_value.clone();
+        let email = email_input_value.clone();
 
         spawn_local(async move {
             let _ = context
@@ -62,27 +72,61 @@ pub fn auth() -> Html {
     html! {
         <div class="hero is-fullheight is-flex is-justify-content-center is-align-content-center is-align-items-center">
             <div class="has-shadow has-radius p-3">
-                <h1 class="is-size-3">{"Sign In"}</h1>
 
-                <form onsubmit={signin}>
-                    <div class="field">
-                        <label class="label">{"Email"}</label>
-                        <div class="control">
-                            <input oninput={email_input_onchange} class="input" type="email" placeholder="username"/>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">{"Password"}</label>
-                        <div class="control">
-                            <input oninput={password_input_onchange} class="input" type="password" placeholder="password" value={password_input_value}/>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control">
-                            <button class="button is-link">{"Login"}</button>
-                        </div>
-                    </div>
-                </form>
+                {
+                    if (*signin_signup) == SignupSignin::Signin {
+                       html!{
+                        <>
+                            <h1 class="is-size-3">{"Sign In"}</h1>
+                            <form onsubmit={signin}>
+                                <div class="field">
+                                    <label class="label">{"Email"}</label>
+                                    <div class="control">
+                                        <input oninput={email_input_onchange} class="input" type="email" placeholder="username"/>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <label class="label">{"Password"}</label>
+                                    <div class="control">
+                                        <input oninput={password_input_onchange} class="input" type="password" placeholder="password" value={password_input_value}/>
+                                    </div>
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-link">{"Login"}</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </>
+                        }
+                    } else {
+                        html!{
+                            <>
+                                <h1 class="is-size-3">{"Sign Up"}</h1>
+                                <form onsubmit={signin}>
+                                    <div class="field">
+                                        <label class="label">{"Email"}</label>
+                                        <div class="control">
+                                            <input oninput={email_input_onchange} class="input" type="email" placeholder="username"/>
+                                        </div>
+                                    </div>
+                                    <div class="field">
+                                        <label class="label">{"Password"}</label>
+                                        <div class="control">
+                                            <input oninput={password_input_onchange} class="input" type="password" placeholder="password" value={password_input_value}/>
+                                        </div>
+                                    </div>
+                                    <div class="field">
+                                        <div class="control">
+                                            <button class="button is-link">{"Login"}</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </>
+                            }
+                    }
+                }
+
             </div>
         </div>
     }
