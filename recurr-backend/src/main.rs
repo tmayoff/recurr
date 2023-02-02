00a550648c1e6823199c76f42a351cd0cc376e9b
Tyler Mayoff <tyler@tylermayoff.com>
2023-02-01 20:36:03 -0500

    Added mobile files

--- recurr-backend/src/main.rs
+++ recurr-backend/src/main.rs
@@ -3,36 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use recurr::AppBuilder;
 
-mod plaid;
-mod supabase;
-
-fn main() {
-    dotenv::dotenv().ok();
-    env_logger::init();
-
-    tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(debug_assertions)] // only include this code on debug builds
-            {
-                let window = app.get_window("main");
-                if let Some(window) = window {
-                    window.open_devtools();
-                }
-            }
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            plaid::accounts::get_balances,
-            plaid::link::link_token_create,
-            plaid::item_public_token_exchange,
-            supabase::access_token::save_access_token,
-            supabase::access_token::get_access_token,
-            supabase::accounts::save_plaid_account,
-            supabase::accounts::get_plaid_accounts,
-            supabase::auth::get_supabase_auth_credentials,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+pub fn main() {
+    AppBuilder::new().run();
 }
